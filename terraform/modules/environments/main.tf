348834ec55386210e4a4c13289b2e63a0e022a0c
Swansen1993 <niederloehnersven@gmail.com>
2026-08-19 20:08:40 +0200

    pipeline angepasst da die rolle für webidentity nicht gefunden wurde

--- terraform/modules/environments/main.tf
+++ terraform/modules/environments/main.tf
@@ -99,6 +99,8 @@ resource "aws_iam_role" "aws_iam_execution_role" {
 }
 
 
+
+
 resource "aws_iam_role_policy_attachment" "ecs_execute" {
   role       = aws_iam_role.aws_iam_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
