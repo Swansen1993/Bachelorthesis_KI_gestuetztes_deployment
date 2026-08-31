cee8980d6da4070e82c648daf614e0608869f959
Swansen1993 <niederloehnersven@gmail.com>
2026-08-31 15:57:43 +0200

    test

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -23,7 +23,7 @@ variable "variant_id" {
 
 variable "target_method" {
   type        = string
-  default     = "pos_001_post_add_article"
+  default     = "pos_001_post_add"
   description = "name der methode die durch den Lasttest getestet wird"
 }
 
