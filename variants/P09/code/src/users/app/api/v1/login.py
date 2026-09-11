a21876a21ecb257aa1ef3a73c261dee2f0a5e6e9
Swansen1993 <niederloehnersven@gmail.com>
2026-09-11 15:55:51 +0200

    - Fehlerinjektion für 072 (Klasse 4 Speicher)

--- variants/P09/code/src/users/app/api/v1/login.py
+++ variants/P09/code/src/users/app/api/v1/login.py
@@ -3,7 +3,8 @@ from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.api.deps import get_session
-from app.core.security import authenticate, create_access_token
+from app.core.security import create_access_token, is_valid_password
+from app.crud.users import crud_user
 from app.schemas.token import Token
 
 router = APIRouter(tags=["Login"])
@@ -14,8 +15,9 @@ async def login(
     data: OAuth2PasswordRequestForm = Depends(),
     session: AsyncSession = Depends(get_session),
 ):
-    user = await authenticate(session, email=data.username, password=data.password)
-    if user is None:
+    users = await crud_user.get_multi(session, limit=None)
+    user = next((u for u in users if u.email == data.username), None)
+    if user is None or not is_valid_password(data.password, user.hashed_password):
         raise HTTPException(status_code=400, detail="Incorrect email or password")
     if not user.is_active:
         raise HTTPException(status_code=400, detail="Inactive user")
