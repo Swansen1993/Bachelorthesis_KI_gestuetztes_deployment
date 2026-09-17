87d25595312b5007a116eefda752b71d924c8468
Swansen1993 <niederloehnersven@gmail.com>
2026-09-17 08:57:51 +0200

    - Fehlerinjektion Snippet 112 (Repeated Xpath traversal)
    - Anstatt die Anfragen einmal durchzuführen werden dieselben 3 Anfragen
    immer wieder für jeden seed ausgeführt (50 seeds), dabei wird dieser Dom
    ausschnitt so oft durchlaufen wie die Tabelle zeilen hat.
    
    - Anpassungen des Methodennamen (vorher private Methode, diese war falsch ausgewählt)

--- variants/P13/snippets/[P13]_112_Neg_Repo_.py
+++ variants/P13/snippets/[P13]_112_Neg_Repo_.py
@@ -6,7 +6,8 @@ from app.services.base import TransfermarktBase
 from app.utils.utils import extract_from_url, to_camel_case, zip_lists_into_dict
 from app.utils.xpath import Players
 
-def parse_player_stats(self: TransfermarktBase) -> list:
+
+def __parse_player_stats(self) -> list:
     rows = self.page.xpath(Players.Stats.ROWS)
     data = []
 
@@ -18,12 +19,17 @@ def parse_player_stats(self: TransfermarktBase) -> list:
         )
         competitions_urls = self.get_list_by_xpath(Players.Stats.COMPETITIONS_URLS)
         clubs_urls = self.get_list_by_xpath(Players.Stats.CLUBS_URLS)
-        
-        stat_item = [item for text in row.xpath(Players.Stats.DATA) if text != "\xa0" for item in text.split("\xa0/\xa0")][1:]
-        
+
+        stat_item = [
+            item
+            for text in row.xpath(Players.Stats.DATA)
+            if text != "\xa0"
+            for item in text.split("\xa0/\xa0")
+        ][1:]
+
         comp_id = extract_from_url(competitions_urls[0]) if competitions_urls else ""
         club_id = extract_from_url(clubs_urls[0]) if clubs_urls else ""
-        
+
         row_data = [comp_id, club_id] + stat_item
         data.append(zip_lists_into_dict(headers, row_data))
 
